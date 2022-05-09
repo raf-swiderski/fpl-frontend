017350c468e1f76f6101281be79525352c8c61d7
Raf Swiderski <rafswid96@gmail.com>
2022-05-09 22:35:15 +0100

    players onto cards

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import { useState, useEffect } from 'react';
-// API => `https://fantasy.premierleague.com/api/${path}`;
 
-// backendURL = 'https://fpl-api-raf.herokuapp.com/';
-// path = `?path=entry/821650/history/`
+import './App.css'
+
+import PlayerCard from './components/PlayerCard';
+
+import * as React from 'react';
+
+import { Grid } from '@mui/material'
+
+
+
 
 function App() {
 
-  const [team, setTeam] = useState('poo');
+  const [team, setTeam] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-      fetch("http://localhost:3000/myteam")
+      fetch("http://localhost:4000/myteam")
         .then(response => response.json())
         .then(response => {
             
@@ -19,20 +26,54 @@ function App() {
           setLoading(false)
         }
       ) 
+
   },[])
 
   if(loading) return <h1>Loading ...</h1>
 
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">Current Team</header>
 
-        <div>GW34 Team</div>  
-        <br></br>
-        <p> {JSON.stringify(team.picks)}</p>
+        <Grid container >
+          <Grid item xs={3} >
+          <PlayerCard player={team[0]} row='1'></PlayerCard> {/* Goalkeepers */}
+          </Grid>
+        </Grid>
+
+
+          <PlayerCard player={team[1]} row='2'></PlayerCard> 
+   
+          <PlayerCard player={team[2]} row='2'></PlayerCard> {/* Defenders */}
+          <PlayerCard player={team[3]} row='2'></PlayerCard>
+          <PlayerCard player={team[4]} row='2'></PlayerCard>
+          <PlayerCard player={team[5]} row='2'></PlayerCard> 
+          <PlayerCard player={team[6]} row='2'></PlayerCard> 
+
+          <PlayerCard player={team[7]} row='3'></PlayerCard> {/* Midfielders */}
+          <PlayerCard player={team[8]} row='3'></PlayerCard> 
+          <PlayerCard player={team[9]} row='3'></PlayerCard> 
+          <PlayerCard player={team[10]} row='3'></PlayerCard> 
+          <PlayerCard player={team[11]} row='3'></PlayerCard> 
+
+          <PlayerCard player={team[12]} row='4'></PlayerCard> {/* Forwards */}
+          <PlayerCard player={team[13]} row='4'></PlayerCard> 
+          <PlayerCard player={team[14]} row='4'></PlayerCard> 
+        
+
+        
+
+      
+
+
+
 
     </div>
   );
 }
+
+
+
+
 
 export default App;
---
+++ src/components/PlayerCard.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { Card, CardContent, Typography } from '@mui/material'
+
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles({
+    root: {
+        minWidth: 275
+    }
+})
+
+function PlayerCard(props) {
+
+    const player = props.player;
+  
+    return (
+      <React.Fragment>    
+        <Card variant="outlined">
+          <CardContent>
+            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+  
+              Player: {props.index + 1}
+  
+            </Typography>
+            <Typography variant="h5" component="div">
+  
+              {player.first_name} • {player.web_name}
+  
+            </Typography>
+            <Typography sx={{ mb: 1.5 }} color="text.secondary">
+  
+              Price: {player.now_cost}
+  
+            </Typography>
+            <Typography variant="body2">
+  
+              Team: {player.team_code}
+              <br />
+              Form: {player.form}
+  
+            </Typography>
+          </CardContent>
+        </Card>
+      </React.Fragment>    
+    )
+}
+
+
+export default PlayerCard;
