297c649617c356fc3da03896dd45171dba560e79
Raf Swiderski <rafswid96@gmail.com>
2022-06-05 00:29:51 +0100

    sorting by position on player list

--- src/components/PlayerSelecter/PlayerSelecter.js
+++ src/components/PlayerSelecter/PlayerSelecter.js
@@ -2,18 +2,37 @@ import { Button, Box } from '@mui/material';
 import * as React from 'react';
 import DynamicList from './DynamicList'
 import ToggleSortBy from './ToggleSortBy';
+import TogglePosition from './TogglePosition';
 
 function sortPlayers(allPlayers, sortBy) {  
   return allPlayers.sort((a, b) => (a[sortBy] < b[sortBy]) ? 1 : -1)
 }
 
+function filterPlayerByPosition(allPlayers, position) {
+  var newPlayers = []
+  console.log(position)
+  
+
+  for (let i = 0; i < allPlayers.length; i++) {
+    if (allPlayers[i].element_type === parseInt(position)) {
+      newPlayers.push(allPlayers[i])
+      console.log(newPlayers)
+      
+    }
+  }
+  console.log(newPlayers)
+  return newPlayers
+}
+
 function PlayerSelecter(props) {
 
-    // if (props.teamID == null) return null
     if (props.allPlayers == null) return null
 
     const [allPlayers, setAllPlayers] = React.useState(props.allPlayers);
     const [sortBy, setSortBy] = React.useState(null);
+    const [position, setPosition] = React.useState(null);
+    const [playersByPosition, setPlayersByPosition] = React.useState(null);
+
 
     const getSortBy = (sortBy) => {
       setSortBy(sortBy)
@@ -24,12 +43,23 @@ function PlayerSelecter(props) {
         setAllPlayers(sorted)
       }
     }
+
+    const getPosition = (position) => {
+      setPosition(position)
+
+      let sorted = filterPlayerByPosition(allPlayers, position)
+      if (sorted != null) {
+        setPlayersByPosition(sorted)
+      } else {
+        setPlayersByPosition(null)
+      }
+    }
   
 
     return (
         <Box sx={{
             bgcolor: 'background',
-            boxShadow: 1,
+            boxShadow: 3,
             borderRadius: 2,
             p: 2,
             marginTop: 3,
@@ -37,8 +67,10 @@ function PlayerSelecter(props) {
           }}>
 
             <ToggleSortBy sortBy={getSortBy}/>
+            <br></br>
+            <TogglePosition position={getPosition}/>
 
-            <DynamicList allPlayers={allPlayers}/>
+            <DynamicList allPlayers={playersByPosition ? playersByPosition : allPlayers}/>
 
 
         </Box>
